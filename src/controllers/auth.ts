49c3a8db99c5351c28adb78e1ca65a09c802337d
mobad <mostafa.m.badawey@gmail.com>
2020-03-15 02:23:17 +0200

    add user login functionality

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -22,7 +22,29 @@ export class AuthController {
                 token
             });
         } catch (err) {
-            console.log(`ERROR in register route: ${err}`)
+            console.log(`ERROR in register controller: ${err}`)
+        }
+    };
+
+    public loginUser = async (req: Request, res: Response) => {
+        try {
+            const user: any = await User.findOne({email: req.body.email});
+            const {email, id, password} = user;
+            const isMatch = await user.comparePassword(req.body.password);
+            if (isMatch) {
+                const token = jwt.sign({
+                    id, email, password
+                }, SECRET_KEY);
+                return res.status(200).json({
+                    email,
+                    id,
+                    token
+                });
+            } else {
+                return res.status(400).json({message: 'Invalid email/password'})
+            }
+        } catch (err) {
+            console.log(`ERROR in login controller: ${err}`)
         }
     }
 }
