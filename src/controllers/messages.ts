b583c885736c420eed8a5a86208983f926191d31
mobad <mostafa.m.badawey@gmail.com>
2020-03-16 18:02:29 +0200

    create getMessages controller

--- src/controllers/messages.ts
+++ src/controllers/messages.ts
@@ -3,6 +3,13 @@ import {Message, IMessage} from "../models/message";
 
 export class MessagesController {
     public getAllMessages = async (req: Request, res: Response) => {
+        try {
+            // @ts-ignore
+            const messages: IMessage[] = await Message.find();
+            return res.status(200).json(messages.map(m => m._doc));
+        } catch (err) {
+            return res.status(400).json(`ERROR on get Messages: ${err.message}`);
+        }
     };
 
     public addMessage = async (req: Request, res: Response) => {
