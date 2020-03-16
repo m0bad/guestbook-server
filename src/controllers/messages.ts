1abe166a08f13068cd07679f7216478506736144
mobad <mostafa.m.badawey@gmail.com>
2020-03-16 17:52:38 +0200

    add create new message controller

--- src/controllers/messages.ts
+++ src/controllers/messages.ts
@@ -1,7 +1,17 @@
 import {Request, Response} from "express";
+import {Message, IMessage} from "../models/message";
 
 export class MessagesController {
-    public getAllMessages = async (req: Request, res: Response) => console.log('OK');
+    public getAllMessages = async (req: Request, res: Response) => {
+    };
 
-    public addMessage = async (req: Request, res: Response) => console.log('OK');
+    public addMessage = async (req: Request, res: Response) => {
+        try {
+            const message: IMessage = new Message(req.body);
+            await message.save();
+            return res.status(200).json(message._doc);
+        } catch (err) {
+            return res.status(400).json(`ERROR on Add Message: ${err.message}`);
+        }
+    };
 }
