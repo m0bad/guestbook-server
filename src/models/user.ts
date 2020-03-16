b822f1598ed1c86ceea843dd736a68684b22b3a5
mobad <mostafa.m.badawey@gmail.com>
2020-03-16 17:34:50 +0200

    edit user model to reference message and replies model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,11 +1,16 @@
 import {Document, model, Model, Schema} from "mongoose";
 import bcrypt from 'bcrypt';
+import {IMessage} from "./message";
+import * as mongoose from "mongoose";
+import {IReply} from "./reply";
 
 export interface IUser extends Document {
     _doc: {
         _id: string,
         email: string,
         password: string,
+        messages?: IMessage[],
+        replies?: IReply[],
     };
     comparePassword(password: string): boolean
 }
@@ -19,7 +24,14 @@ export const userSchema: Schema = new Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    messages: [{
+        type: mongoose.Schema.Types.ObjectId,
+    }],
+
+    replies: [{
+        type: mongoose.Schema.Types.ObjectId,
+    }],
 });
 
 userSchema.pre<IUser>('save', function save(next) {
