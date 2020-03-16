c9c51852fa757633dcf636d5a074819fdee3c8b0
mobad <mostafa.m.badawey@gmail.com>
2020-03-16 17:34:14 +0200

    define message model

---
+++ src/models/message.ts
@@ -0,0 +1,41 @@
+import {Document, model, Model, Schema} from "mongoose";
+import {IUser} from "./user";
+import * as mongoose from "mongoose";
+import {IReply} from "./reply";
+
+export interface IMessage extends Document {
+    _doc: {
+        _id: string,
+        text: string,
+        author: IUser,
+        replies?: IReply[],
+        createdAt: Date,
+        updatedAt: Date,
+    };
+}
+
+export const messageSchema: Schema = new Schema({
+    text: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    replies: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+
+export const Message: Model<IMessage> = model<IMessage>('Message', messageSchema);
